2abb64e4350fc405b14f45eabdff4920b2555494
Erano01 <muhammedalisert01@gmail.com>
2026-04-20 02:50:02 +0300

    feat: harmony patch for Doctor perk floor enforcement

--- Src/Application.cs
+++ Src/Application.cs
@@ -8,6 +8,8 @@ namespace Harmony
         {
             var harmony = new HarmonyLib.Harmony(_modInstance.Name);
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+            StarterKits.Harmony.DoctorPerkFloorPatch.Register(harmony);
+            Log.Out($"[StarterKits] Harmony patches applied from assembly: {Assembly.GetExecutingAssembly().FullName}");
 
             StarterKits.PlayerJoinedGameHandler.Init();
             // XUi controller tipimizin assembly'yi yüklerken canlı olduğundan emin olalım
